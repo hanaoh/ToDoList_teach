eb2acbf8a1c318ab8e42bfa6f8b207466487b26c
a0972199950 <a0972199950@gmail.com>
2018-12-04 12:18:30 +0800

    20181204課堂

---
+++ app.js
@@ -0,0 +1,11 @@
+// $(css選擇器).事件監聽器
+//            .取用元素內部內容
+
+// $("#header").attr("class", "top");
+
+// $().on("監聽的事件內容", "要執行的函數")
+
+// inputArea裡面的(空格)button元素
+// $("#inputArea button").on("mouseover", function(){
+//     alert("我被點了");
+// })
